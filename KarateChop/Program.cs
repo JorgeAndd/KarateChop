65f60fe7d5b122d8d07d731743c36d6fb049b23f
Jorge Luiz Andrade <jorgeluizandrade@outlook.com>
2016-06-21 23:09:32 -0300

    First chop implementation using simple index manipulation function

---
+++ KarateChop/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KarateChop
+{
+	public abstract class Karate
+	{
+		public abstract int Chop(int n, int[] array);
+	}
+
+
+	// First implementation of binary search
+	public class SimpleKarate : Karate
+	{
+		// Binary search using indices manipulation
+		public override int Chop(int n, int[] array)
+		{
+
+			int start = 0;
+			int lenght = array.Length;
+			int end = lenght - 1;
+
+			while(start <= end)
+			{
+				int mid = ((end - start) / 2) + start;
+
+				if (array[mid] == n)
+					return mid;
+
+				if(array[mid] > n)
+				{
+					end = mid - 1;
+				}
+				else
+				{
+					start = mid + 1;
+				}
+			}
+
+			return -1;
+		}
+	}
+
+	public class Program
+	{
+		static int Chop(int n, int[] array)
+		{
+			return -1;
+		}
+
+		static void Main(string[] args)
+		{
+			SimpleKarate simpleKarate = new SimpleKarate();
+
+			simpleKarate.Chop(0, new int[] {1, 3, 5});
+		}	
+	}
+}
+
